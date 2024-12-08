821d65c5c25f779c775011d9fb67f1aa8a72e9a0
wsantos <wsm007@gmail.com>
2024-12-08 18:50:09 -0500

    Creación de autenticación, SingIn, SingOut, SingUp y Profile

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -2,14 +2,43 @@ import bcrypt from 'bcrypt'
 import { pool } from '../db.js'
 import { createAccessToken } from '../libs/jwt.js'
 
-export const signIn = (req, res) => res.send('Ingresando');
+export const signIn = async (req, res) => {
+    const { email, password } = req.body
 
-export const signUp = async (req, res) => {
+    const result = await pool.query('SELECT * FROM users WHERE email = $1', [email])
+
+    if (result.rowCount === 0) {
+        return res.status(400).json({
+            message: 'El correo no está registrado'
+        })
+    }
+
+    const validPassword = await bcrypt.compare(password, result.rows[0].password)
+
+    if (!validPassword) {
+        return res.status(400).json({
+            message: 'La contraseña es incorrecta'
+        })
+    }
+
+    const token = await createAccessToken({ id: result.rows[0].id })
+
+    res.cookie('token', token, {
+        httpOnly: true,
+        // secure: true,
+        sameSite: 'none',
+        maxAge: 24 * 60 * 60 * 1000
+    })
+
+    return res.json(result.rows[0])
+}
+
+export const signUp = async (req, res, next) => {
     const { name, email, password } = req.body;
 
     try {
         const hashedPassword = await bcrypt.hash(password, 10)
-        
+
         const result = await pool.query('INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *', [name, email, hashedPassword])
 
         const token = await createAccessToken({ id: result.rows[0].id })
@@ -29,9 +58,17 @@ export const signUp = async (req, res) => {
             })
         }
 
+        next(error);
     };
 };
 
-export const signOut = (req, res) => res.send('Cerrando sesion');
+export const signOut = (req, res) => {
+    res.clearCookie('token');
+    res.sendStatus(200);
+}
 
-export const profile = (req, res) => res.send('Cerrando sesion');
+export const profile = async (req, res) => {
+
+    const result = await pool.query('SELECT * FROM users WHERE id = $1', [req.userId])
+    return res.json(result.rows[0]);
+}
